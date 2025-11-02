35ae0f85e2746c58c00dd49d08f7ff3bc436c41d
prateekraiger <prateekraiger123@gmail.com>
2025-11-02 15:54:15 +0530

    add config files for auth

---
+++ server/users.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+
+export const signInUser = async (email: string, password: string) => {
+  try {
+    await auth.api.signInEmail({
+      body: {
+        email,
+        password,
+      },
+    });
+    return { success: true, message: "sign in successfully" };
+  } catch (error) {
+    const e = error as Error;
+    return { success: false, message: e.message || "Failed to sign up" };
+  }
+};
+
+export const signUpUser = async (
+  email: string,
+  password: string,
+  name: string,
+) => {
+  try {
+    await auth.api.signUpEmail({
+      body: {
+        email,
+        password,
+        name,
+      },
+    });
+    return { success: true, message: "Signed up successfully" };
+  } catch (error) {
+    const e = error as Error;
+
+    return { success: false, message: e.message || "Failed to sign up" };
+  }
+};
