8bdd3a269a9052c79e428f732c114a17b9a702f6
prateekraiger <prateekraiger123@gmail.com>
2025-11-10 09:11:09 +0530

    updated hero section

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -5,7 +5,6 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { TextEffect } from "@/components/ui/text-effect";
 import { AnimatedGroup } from "@/components/ui/animated-group";
-import { HeroHeader } from "./header";
 
 const transitionVariants = {
   item: {
@@ -30,7 +29,6 @@ const transitionVariants = {
 export default function HeroSection() {
   return (
     <>
-      <HeroHeader />
       <main className="overflow-hidden">
         <div
           aria-hidden
